4bc30d936f2d669ea1c998ab373fb041e05181a8
Gabriel Souza <gabrie.ssouza@gmail.com>
2019-09-29 09:47:40 -0300

    chore(files): reorganize files

--- backend/scripts/updateSchema.js
+++ backend/scripts/updateSchema.js
@@ -30,6 +30,10 @@ import { introspectionQuery, printSchema } from 'graphql/utilities';
       path.join(__dirname, '../data/schema.json'),
       JSON.stringify(result, null, 2)
     );
+    fs.writeFileSync(
+      path.join(__dirname, '../../frontend/data/schema.json'),
+      JSON.stringify(result, null, 2)
+    );
 
     process.exit(0);
   }
